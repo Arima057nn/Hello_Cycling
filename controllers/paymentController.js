fbe706f62c69d681a74a732c0f53352877791461
Arima057nn <tjendung97pain@gmail.com>
2024-05-25 20:32:08 +0700

    check transaction status + callback

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,106 +1,17 @@
 const axios = require("axios");
-
-const Payment = async (req, res) => {
-  var accessKey = process.env.MOMO_ACCESS_KEY;
-  var secretKey = process.env.MOMO_SECRET_KEY;
-  var orderInfo = "pay with MoMo";
-  var partnerCode = "MOMO";
-  var redirectUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
-  var ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
-  var requestType = "payWithMethod";
-  var amount = "50000";
-  var orderId = partnerCode + new Date().getTime();
-  var requestId = orderId;
-  var extraData = "";
-  var orderGroupId = "";
-  var autoCapture = true;
-  var lang = "vi";
-
-  //before sign HMAC SHA256 with format
-  //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
-  var rawSignature =
-    "accessKey=" +
-    accessKey +
-    "&amount=" +
-    amount +
-    "&extraData=" +
-    extraData +
-    "&ipnUrl=" +
-    ipnUrl +
-    "&orderId=" +
-    orderId +
-    "&orderInfo=" +
-    orderInfo +
-    "&partnerCode=" +
-    partnerCode +
-    "&redirectUrl=" +
-    redirectUrl +
-    "&requestId=" +
-    requestId +
-    "&requestType=" +
-    requestType;
-  //puts raw signature
-  console.log("--------------------RAW SIGNATURE----------------");
-  console.log(rawSignature);
-  //signature
-  const crypto = require("crypto");
-  var signature = crypto
-    .createHmac("sha256", secretKey)
-    .update(rawSignature)
-    .digest("hex");
-  console.log("--------------------SIGNATURE----------------");
-  console.log(signature);
-
-  //json object send to MoMo endpoint
-  const requestBody = JSON.stringify({
-    partnerCode: partnerCode,
-    partnerName: "Test",
-    storeId: "MomoTestStore",
-    requestId: requestId,
-    amount: amount,
-    orderId: orderId,
-    orderInfo: orderInfo,
-    redirectUrl: redirectUrl,
-    ipnUrl: ipnUrl,
-    lang: lang,
-    requestType: requestType,
-    autoCapture: autoCapture,
-    extraData: extraData,
-    orderGroupId: orderGroupId,
-    signature: signature,
-  });
-  // options  for axios
-  const options = {
-    method: "POST",
-    url: "https://test-payment.momo.vn/v2/gateway/api/create",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(requestBody),
-    },
-    data: requestBody,
-  };
-
-  //send request
-  let result;
-  try {
-    result = await axios(options);
-    return res.status(200).json(result.data);
-  } catch (errror) {
-    return res.status(400).json({ error: error.message });
-  }
-};
+const crypto = require("crypto");
 
 const Momo = async (req, res) => {
-  var partnerCode = "MOMO";
+  const { amountOfMoney } = req.body;
+  var partnerCode = process.env.MOMO_PARTNER_CODE;
   var accessKey = process.env.MOMO_ACCESS_KEY;
   var secretkey = process.env.MOMO_SECRET_KEY;
   var requestId = partnerCode + new Date().getTime();
   var orderId = requestId;
   var orderInfo = "pay with MoMo";
-  var redirectUrl = "https://momo.vn/return";
-  var ipnUrl = "https://callback.url/notify";
-  // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
-  var amount = "50000";
+  var redirectUrl = process.env.MOMO_REDIRECT_URL;
+  var ipnUrl = `${process.env.MOMO_IPN_URL}/api/payment/callback`;
+  var amount = amountOfMoney;
   var requestType = "captureWallet";
   var extraData = ""; //pass empty value if your merchant does not have stores
 
@@ -131,7 +42,7 @@ const Momo = async (req, res) => {
   console.log("--------------------RAW SIGNATURE----------------");
   console.log(rawSignature);
   //signature
-  const crypto = require("crypto");
+
   var signature = crypto
     .createHmac("sha256", secretkey)
     .update(rawSignature)
@@ -156,7 +67,7 @@ const Momo = async (req, res) => {
   });
   const options = {
     method: "POST",
-    url: "https://test-payment.momo.vn/v2/gateway/api/create",
+    url: `${process.env.MOMO_URL}/v2/gateway/api/create`,
     headers: {
       "Content-Type": "application/json",
       "Content-Length": Buffer.byteLength(requestBody),
@@ -173,7 +84,43 @@ const Momo = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
+
+const Callback = async (req, res) => {
+  console.log("callback", req.body);
+  return res.status(200).json({ message: "success", data: req.body });
+};
+
+const TransactionStatus = async (req, res) => {
+  const { orderId } = req.body;
+  const rawSignature = `accessKey=${process.env.MOMO_ACCESS_KEY}&orderId=${orderId}&partnerCode=${process.env.MOMO_PARTNER_CODE}&requestId=${orderId}`;
+
+  const signature = crypto
+    .createHmac("sha256", process.env.MOMO_SECRET_KEY)
+    .update(rawSignature)
+    .digest("hex");
+
+  const requestBody = JSON.stringify({
+    partnerCode: process.env.MOMO_PARTNER_CODE,
+    requestId: orderId,
+    orderId,
+    signature,
+    lang: "vi",
+  });
+
+  const options = {
+    method: "POST",
+    url: `${process.env.MOMO_URL}/v2/gateway/api/query`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: requestBody,
+  };
+  let result = await axios(options);
+
+  return res.status(200).json(result.data);
+};
 module.exports = {
-  Payment,
   Momo,
+  Callback,
+  TransactionStatus,
 };
--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { Payment, Momo } = require("../controllers/paymentController");
+const {
+  Momo,
+  Callback,
+  TransactionStatus,
+} = require("../controllers/paymentController");
 
 const router = express.Router();
 
-router.post("/", Payment);
 router.post("/momo", Momo);
+router.post("/callback", Callback);
+router.post("/transaction-status", TransactionStatus);
 
 module.exports = router;
